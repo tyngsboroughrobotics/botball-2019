22d104b629f3adb4c10844104a8ccd6823a2c4ce
Wilson Gramer <wgramer03@gmail.com>
2019-03-12 14:01:18 -0400

    WIP

--- src/game/game.py
+++ src/game/game.py
@@ -7,12 +7,19 @@ except ImportError:
 from src.helpers.motors import motor 
 from src.helpers.motors import servo
 from src.helpers.cameras import camera as _camera
+from src.helpers.create import create
 
 # Set up motors/servos
 
-left_motor = motor.motor(port=1, speed=1.0)
-right_motor = motor.motor(port=3, speed=1.0)
-wheels = motor.wheel_group(left_motor, right_motor)
+# the right motor veers off a bit, so we make it a bit faster to have the
+# robot move in a straight line
+LEFT_MOTOR_OFFSET = 1
+RIGHT_MOTOR_OFFSET = 1.035
+WHEEL_SPEED = 0.5
+
+left_motor = motor.motor(port=1, speed=(WHEEL_SPEED * LEFT_MOTOR_OFFSET))
+right_motor = motor.motor(port=3, speed=(WHEEL_SPEED * RIGHT_MOTOR_OFFSET)) 
+wheels = motor.wheel_group(left_motor, right_motor, LEFT_MOTOR_OFFSET, RIGHT_MOTOR_OFFSET)
 
 arm_servo = servo.servo(port=3, speed=0.7)
 
@@ -36,21 +43,31 @@ def step_1_get_firefighter():
 
     # From the starting box, turn and face the firefighter cube. Put the plow down and drive it over
     # to the firefighter cube so the cube is in the plow
-
     wheels.turn_left(20) # in degrees
     arm_servo.set_position(0.35) # 0.4 makes the plow touch the table (don't set it to more than this)
-    wheels.drive(500) # in mm
+    wheels.drive(100) # in mm
+    wheels.turn_left(70)
 
-    # Drive over to the burning buildings
+    print '**** Step 1 done ****'
 
-    for _ in range(3):
-        wheels.drive(2000)
-        wheels.turn_left(15)
+# Drive over with the cube to the burning buildings
+def step_2_drive_over_to_buildings():
+    print '**** Step 2: Drive over to buildings'
 
-    wheels.turn_left(50)
-    wheels.drive(1000)
+    arm_servo.set_position(0.35) # TEMPORARY
 
-    # Dispense the cube in the burning building
+    wheels.drive(350)
+    wheels.turn_right(45)
+    wheels.drive(200)
+    wheels.turn_right(45)
+    wheels.drive(300)
+    wheels.turn_left(90)
+
+    print '**** Step 2 done ****'
+
+# Check which building is the burning one, and dispense the cube in front of it
+def step_3_put_cube_in_burning_building():
+    print '**** Step 3: Put firefighter in burning building **'
 
     with _camera.camera(color='yellow') as camera:
         if camera.object_is_present() and camera.is_current_object_trackable():
@@ -58,10 +75,45 @@ def step_1_get_firefighter():
         else:
             pass # Move to the second building and dispense it there
 
-    print '**** Step 1 done ****'
+    print '**** Step 3 done ****'
 
 def run():
     print '**** Running game ****'
 
-    reset()
-    step_1_get_firefighter()
+    # reset()
+ 
+    # # step_1_get_firefighter()
+    # step_2_drive_over_to_buildings()
+
+    # finish()
+
+    while True:
+        wheels.drive(3000, block=True)
+        wheels.turn_right(90, block=True)
+
+    # while True:
+    #     w.motor(1, 90)
+    #     w.motor(3, 90)
+    #     w.msleep(1000)
+    #     w.ao()
+    #     w.msleep(3000)
+
+    # LEFT_WHEEL = 1
+    # RIGHT_WHEEL = 3
+    # MOTOR_SPEED = 750 # half-speed
+
+    # while True:
+    #     print 'forward'
+    #     w.move_relative_position(LEFT_WHEEL, MOTOR_SPEED, 1000)
+    #     w.move_relative_position(RIGHT_WHEEL, MOTOR_SPEED, 1000)
+    #     w.block_motor_done(RIGHT_WHEEL)
+    #     print 'forward done'
+    #     w.ao()
+    #     print 'turn'
+    #     w.move_relative_position(LEFT_WHEEL, MOTOR_SPEED, 500)
+    #     w.move_relative_position(RIGHT_WHEEL, MOTOR_SPEED, -500)
+    #     w.block_motor_done(RIGHT_WHEEL)
+    #     print 'turn done'
+    #     w.ao()
+
+    print '**** Done game ****'
--- src/helpers/motors/motor.py
+++ src/helpers/motors/motor.py
@@ -8,6 +8,7 @@ from base_motor import base_motor
 from src.helpers.functions import map
 
 TICKS_IN_1_MM = 1.0/1.13  # looks weird, but we did the measurement :)
+MAX_MOTOR_SPEED = 1500  # in ticks/second
 
 FORWARD = 0
 BACKWARD = 1
@@ -15,12 +16,16 @@ BACKWARD = 1
 
 class motor(base_motor):
 
-  def __wait_until_done(self):
-    while not w.get_motor_done(self.port):
-      w.msleep(2)
-
   def __mm_to_ticks(self, mm):
-    return TICKS_IN_1_MM * mm
+    return TICKS_IN_1_MM * mm * 10
+
+  def __ticks_to_ms(self, ticks):
+    h = int((abs(ticks) / self.__get_velocity()))
+    print '^^^^', h
+    return h
+
+  def __get_velocity(self):
+    return int(map(self.speed, 0.0, 1.0, 0, MAX_MOTOR_SPEED))
 
   def move(self, direction, distance, block=False):
     """Moves the motor.
@@ -31,45 +36,62 @@ class motor(base_motor):
       block {bool} -- Whether to block the thread until finished. (Default: `False`)
     """
 
-    velocity = map(self.speed, 0.0, 1.0, 0, 1000)
+    velocity = self.__get_velocity()
     distance_in_ticks = self.__mm_to_ticks(distance)
 
-    print '***', distance, '==>', distance_in_ticks
-
-    w.move_relative_position(self.port, int(velocity), int(-distance_in_ticks if direction == BACKWARD else distance_in_ticks))
+    w.mav(self.port, velocity)
 
     if block:
-      w.block_motor_done(self.port)
+        w.msleep(self.__ticks_to_ms(distance_in_ticks))
+        self.off()
+
+  def off(self):
+    w.off(self.port)
+
+def all_motors_off():
+    w.ao()
 
 
 # Some helper functions
 
-TURN_DEGREE_AMOUNT = 10 # amount in mm (sent to the wheel_group.drive method)
-
+TURN_DEGREE_AMOUNT = 1 # amount in mm (sent to the wheel_group.drive method)
 
 class wheel_group(object):
     """Represents a group of two motors representing a left
     and right wheel.
     """
 
-    def __init__(self, left, right):
+    def __init__(self, left, right, left_offset = 1, right_offset = 1):
         self.left_motor = left
         self.right_motor = right
+        self.left_offset = left_offset
+        self.right_offset = right_offset
 
-    def drive(self, left_distance, right_distance = None, direction = FORWARD, block = True):
+    def drive(self, left_distance, right_distance = None, direction = FORWARD, block = True, offset = True):
         """
         Drives both motors and blocks (by default) until they finish.
 
         Amount is in mm. If `right_distance` is omitted then
         `left_distance` is used for both motors.
+
+        If you don't `block`, then you're responsible for calling `off()` 
+        on the motors you drive!
         """
-        self.left_motor.move(direction, left_distance)
-        self.right_motor.move(direction, (right_distance if right_distance is not None else left_distance), block=block)
+
+        ld = left_distance
+        rd = right_distance if right_distance is not None else left_distance
+
+        if offset:
+            ld *= self.left_offset
+            rd *= self.right_offset
+
+        self.left_motor.move(direction, ld)
+        self.right_motor.move(direction, rd, block=block)
 
     # Helpers for turning in place
 
-    def turn_right(self, degrees):
-        self.drive(TURN_DEGREE_AMOUNT * degrees, -TURN_DEGREE_AMOUNT * degrees)
+    def turn_right(self, degrees, block = True):
+        self.drive(TURN_DEGREE_AMOUNT * degrees, -TURN_DEGREE_AMOUNT * degrees, offset=False, block=block)
 
-    def turn_left(self, degrees):
-        self.turn_right(-degrees)
+    def turn_left(self, degrees, block = True):
+        self.turn_right(-degrees, block=block)
